4b401b16150364772821cf0e9bfdc42f71eb9f2b
AndreaCatapano <a.catapano96@gmail.com>
2025-02-25 14:06:50 +0100

    feat: updated the app.js file for clearer readability and fixed some punctuation errors.

--- js/app.js
+++ js/app.js
@@ -1,14 +1,18 @@
 for (let i = 1; i <= 100; i++) {   //Per ogni numero da 1 (indice) a 100 (condizione)
-    if(i % 3 === 0 && i % 5 === 0){  //SE l'indice è un numero divisibile per 3 e 5 
-        console.log("fizzbuzz") //Stampa "BUZZFIZZ"
+   
+    const isDivisibleBy3 = i % 3 === 0;
+    const isDivisibleBy5 = i % 5 === 0;
+
+    if (isDivisibleBy3 && isDivisibleBy5) {  //SE l'indice è un numero divisibile per 3 e 5 
+        console.log("fizzbuzz"); //Stampa "BUZZFIZZ"
     }
-    else if(i % 5 === 0){ // ALTRIMENTI SE l'indice un numero divisibile per 5 
-        console.log("buzz") //Stampa "BUZZ"
+    else if (isDivisibleBy5) { // ALTRIMENTI SE l'indice un numero divisibile per 5 
+        console.log("buzz"); //Stampa "BUZZ"
     }
-    else if(i % 3 === 0){ //ALTRIMENTI SE se l'indice è un numero divisibile per 3
-        console.log("fizz") //Stampa "FIZZ"
+    else if (isDivisibleBy3) { //ALTRIMENTI SE se l'indice è un numero divisibile per 3
+        console.log("fizz"); //Stampa "FIZZ"
     }
-    else{
-        console.log(i) // Stampa l'indice
+    else {
+        console.log(i); // Stampa l'indice
     }
 }
