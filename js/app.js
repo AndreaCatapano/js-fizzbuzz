3d1d6923c0c8b436ad7a4591cd347bcf6b9f8a02
AndreaCatapano <a.catapano96@gmail.com>
2025-02-25 13:57:57 +0100

    feat: added "fizzbuzz" check in the cicle for

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
 for (let i = 1; i <= 100; i++) {   //Per ogni numero da 1 (indice) a 100 (condizione)
-    console.log(i)
+    if(i % 3 === 0 && i % 5 === 0){  //SE l'indice è un numero divisibile per 3 e 5 
+        console.log("fizzbuzz") //Stampa "BUZZFIZZ"
+    }
+    else{
+        console.log(i) //Stampa l'indice
+    }
 }
