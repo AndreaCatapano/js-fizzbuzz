09908a496f60f2ecbd376c775474bc12bc9be879
AndreaCatapano <a.catapano96@gmail.com>
2025-02-25 14:00:30 +0100

    feat: added "buzz" check and "fizz" check in the loop

--- js/app.js
+++ js/app.js
@@ -2,7 +2,13 @@ for (let i = 1; i <= 100; i++) {   //Per ogni numero da 1 (indice) a 100 (condiz
     if(i % 3 === 0 && i % 5 === 0){  //SE l'indice è un numero divisibile per 3 e 5 
         console.log("fizzbuzz") //Stampa "BUZZFIZZ"
     }
+    else if(i % 5 === 0){ // ALTRIMENTI SE l'indice un numero divisibile per 5 
+        console.log("buzz") //Stampa "BUZZ"
+    }
+    else if(i % 3 === 0){ //ALTRIMENTI SE se l'indice è un numero divisibile per 3
+        console.log("fizz") //Stampa "FIZZ"
+    }
     else{
-        console.log(i) //Stampa l'indice
+        console.log(i) // Stampa l'indice
     }
 }
